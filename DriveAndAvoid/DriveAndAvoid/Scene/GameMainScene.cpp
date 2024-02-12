c24c5b6f0d52692ad63370e42e1d64b2ea6acbf4
kamizato1 <106945305+kamizato1@users.noreply.github.com>
2024-02-12 15:48:11 +0900

    背景画像だけ変えました

--- DriveAndAvoid/DriveAndAvoid/Scene/GameMainScene.cpp
+++ DriveAndAvoid/DriveAndAvoid/Scene/GameMainScene.cpp
@@ -24,7 +24,7 @@ void GameMainScene::Initialize()
 	ReadHighScore();
 
 	//画像の読み込み
-	back_ground = LoadGraph("Resource/images/back.bmp");
+	back_ground = LoadGraph("Resource/images/background.png");
 	barrier_image = LoadGraph("Resource/images/barrier.png");
 	int result = LoadDivGraph("Resource/images/car.bmp", 3, 3, 1, 63, 120, enemy_image);
 
@@ -62,7 +62,7 @@ eSceneType GameMainScene::Update()
 	player->Update();
 
 	//移動距離の更新
-	mileage += (int)player->GetSpped() + 5;
+	mileage++;
 
 	//敵生成処理
 	if (mileage / 20 % 100 == 0)
@@ -120,8 +120,8 @@ eSceneType GameMainScene::Update()
 void GameMainScene::Draw() const
 {
 	//背景画像の描画
-	DrawGraph(0, mileage % 480 - 480, back_ground, TRUE);
-	DrawGraph(0, mileage % 480, back_ground, TRUE);
+	DrawGraph(-mileage % 900, 0, back_ground, TRUE);
+	DrawGraph(-mileage % 900 + 900, 0, back_ground, TRUE);
 
 	//敵の描画
 	for (int i = 0; i < 10; i++)
@@ -136,39 +136,39 @@ void GameMainScene::Draw() const
 	player->Draw();
 
 	//UIの描画
-	DrawBox(500, 0, 640, 480, GetColor(0, 153, 0), TRUE);
-	SetFontSize(16);
-	DrawFormatString(510, 20, GetColor(0, 0, 0), "ハイスコア");
-	DrawFormatString(560, 40, GetColor(255, 255, 255), "%08d", high_score);
-	DrawFormatString(510, 80, GetColor(0, 0, 0), "避けた数");
-	for (int i = 0; i < 3; i++)
-	{
-		DrawRotaGraph(523 + (i * 50), 120, 0.3, 0, enemy_image[i], TRUE, FALSE);
-		DrawFormatString(510 + (i * 50), 140, GetColor(255, 255, 255), "%03d", enemy_count[i]);
+	//DrawBox(0, 0, 640, 480, GetColor(0, 153, 0), TRUE);
+	//SetFontSize(16);
+	//DrawFormatString(510, 20, GetColor(0, 0, 0), "ハイスコア");
+	//DrawFormatString(560, 40, GetColor(255, 255, 255), "%08d", high_score);
+	//DrawFormatString(510, 80, GetColor(0, 0, 0), "避けた数");
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	DrawRotaGraph(523 + (i * 50), 120, 0.3, 0, enemy_image[i], TRUE, FALSE);
+	//	DrawFormatString(510 + (i * 50), 140, GetColor(255, 255, 255), "%03d", enemy_count[i]);
 
-	}
-	DrawFormatString(510, 200, GetColor(0, 0, 0), "走行距離");
-	DrawFormatString(555, 220, GetColor(255, 255, 255), "%08d", mileage / 10);
-	DrawFormatString(510, 240, GetColor(0, 0, 0), "スピード");
-	DrawFormatString(555, 260, GetColor(255, 255, 255), "%08.1f", player->GetSpped());
+	//}
+	//DrawFormatString(510, 200, GetColor(0, 0, 0), "走行距離");
+	//DrawFormatString(555, 220, GetColor(255, 255, 255), "%08d", mileage / 10);
+	//DrawFormatString(510, 240, GetColor(0, 0, 0), "スピード");
+	//DrawFormatString(555, 260, GetColor(255, 255, 255), "%08.1f", player->GetSpped());
 
-	//バリア枚数の描画
-	for (int i = 0; i < player->GetBarrierCount(); i++)
-	{
-		DrawRotaGraph(520 + i * 25, 340, 0.2f, 0, barrier_image, TRUE, FALSE);
-	}
+	////バリア枚数の描画
+	//for (int i = 0; i < player->GetBarrierCount(); i++)
+	//{
+	//	DrawRotaGraph(520 + i * 25, 340, 0.2f, 0, barrier_image, TRUE, FALSE);
+	//}
 
-	//燃料ゲージの描画
-	float fx = 510.0f;
-	float fy = 390.0f;
-	DrawFormatStringF(fx, fy, GetColor(0, 0, 0), "FUEL METER");
-	DrawBoxAA(fx, fy + 20.0f, fx + 100.0f, fy + 40.0f, GetColor(0, 0, 0), FALSE);
+	////燃料ゲージの描画
+	//float fx = 510.0f;
+	//float fy = 390.0f;
+	//DrawFormatStringF(fx, fy, GetColor(0, 0, 0), "FUEL METER");
+	//DrawBoxAA(fx, fy + 20.0f, fx + 100.0f, fy + 40.0f, GetColor(0, 0, 0), FALSE);
 
-	//体力ゲージの描画
-	fx = 510.0f;
-	fy = 430.0f;
-	DrawFormatStringF(fx, fy, GetColor(0, 0, 0), "PLAYER_HP");
-	DrawBoxAA(fx, fy + 20.0f, fx + 100.0f, fy + 40.0f, GetColor(0, 0, 0), FALSE);
+	////体力ゲージの描画
+	//fx = 510.0f;
+	//fy = 430.0f;
+	//DrawFormatStringF(fx, fy, GetColor(0, 0, 0), "PLAYER_HP");
+	//DrawBoxAA(fx, fy + 20.0f, fx + 100.0f, fy + 40.0f, GetColor(0, 0, 0), FALSE);
 }
 
 //終了時処理
