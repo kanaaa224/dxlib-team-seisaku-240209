158055081f5b2edff548bf3edbb67af922f8bf00
kamizato1 <106945305+kamizato1@users.noreply.github.com>
2024-02-14 14:04:25 +0900

    敵の回転

--- DriveAndAvoid/DriveAndAvoid/Object/Enemy.cpp
+++ DriveAndAvoid/DriveAndAvoid/Object/Enemy.cpp
@@ -1,7 +1,9 @@
 #include"Enemy.h"
 #include"DxLib.h"
 
-Enemy::Enemy(int image_handle, int type) : image_handle(image_handle), type(type), hp(3), speed(0.0f),location(Vector2D(0.0f,0.0f)), box_size(Vector2D(40.0f, 40.0f))
+#define PI 3.14159	// ‰~Žü—¦
+
+Enemy::Enemy(int image_handle, int type) : image_handle(image_handle), type(type), speed(0.0f),location(Vector2D(0.0f,0.0f)), box_size(Vector2D(40.0f, 40.0f)),angle(0)
 {
 	speed = GetRand(3) + 1;
 	location = Vector2D(1000 + GetRand(200), GetRand(480));
@@ -15,12 +17,13 @@ Enemy::~Enemy()
 void Enemy::Update()
 {
 	location.x -= speed;
+	if ((angle -= 5) < -360)angle = 0;
 }
 
 //•`‰æˆ—
 void Enemy::Draw()
 {
-	DrawRotaGraph(location.x + (box_size.x / 2), location.y + (box_size.y / 2), 0.2, 0, image_handle, true);
+	DrawRotaGraph(location.x + (box_size.x / 2), location.y + (box_size.y / 2), 0.2, PI / 180 * angle, image_handle, true);
 	DrawBox(location.x, location.y, location.x + box_size.x, location.y + box_size.y, 0xffffff, false);
 }
 
@@ -31,13 +34,4 @@ Vector2D Enemy::GetLocation() const
 Vector2D Enemy::GetBoxSize() const
 {
 	return box_size;
-}
-int Enemy::GetHp() const
-{
-	return hp;
-}
-
-void Enemy::SetHp(int hp)
-{
-	this->hp = hp;
 }
