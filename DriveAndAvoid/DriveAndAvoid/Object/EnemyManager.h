f56e205c07fffd717c896a5b0c3ccf357d65bf00
kamizato1 <106945305+kamizato1@users.noreply.github.com>
2024-02-15 16:35:34 +0900

    エネミーマネージャー

--- DriveAndAvoid/DriveAndAvoid/Object/EnemyManager.h
+++ DriveAndAvoid/DriveAndAvoid/Object/EnemyManager.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include"Enemy.h"
+#include"Player.h"
+
+class EnemyManager
+{
+private:
+	char comments[COMMENT_TYPE][50];
+	Comment** normal_comment;//普通のサイズのコメント
+	Comment** big_comment;//大きいサイズのコメント
+
+public:
+	CommentManager();
+	~CommentManager();
+
+	void Update(Player* player); // 更新処理
+	void Draw() const;        // 描画処理
+	void CommentGenerate();
+	bool HitComment(Collider* collider, bool can_delete);
+	bool HitNormalComment(Collider* collider, bool can_delete);
+	bool HitBigComment(Collider* collider, bool can_delete);
+};
